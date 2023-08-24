88ffa1b35ba6175ad624594636e60bc0cf57b725
CristhianBarrientos <109832654+CristhianBarrientos@users.noreply.github.com>
2023-08-23 23:09:52 -0600

    Add files via upload

---
+++ consultaMurcielago/src/consultamurcielago/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package consultamurcielago;
+
+/**
+ *
+ * @author Cristhian Barrientos
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        frmConsulta frm = new frmConsulta();
+        frm.show();
+        // new frmConsulta();
+    }
+
+}
